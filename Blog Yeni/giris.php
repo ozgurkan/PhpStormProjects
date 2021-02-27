8786c1fd936825232185f03a4a6e0d115822682e
Özgür KAN <galatasaray_ozgur_94@hotmail.com>
2021-02-27 14:42:03 +0300

    Add files via upload

---
+++ Blog Yeni/giris.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Özgür-KAN
+ * Date: 26.07.2014
+ * Time: 02:01
+ */
+
+?>
+
+<!doctype html>
+    <head>
+        <link href="css/reset.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body background="images/arkaplan.jpg">
+        <div id="genel" style="width:900px;height: 600px;margin:150px auto;background-color: coral">
+            <div id="ust" style="margin: 0 auto;">
+                <img src="images/ust.png">
+            </div>
+
+        </div>
+
+    </body>
+
+
+</html>
